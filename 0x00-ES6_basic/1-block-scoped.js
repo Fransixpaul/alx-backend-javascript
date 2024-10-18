18ce9571405fadb27d2fd2ac4294609882093bd2
francis <krypton@gmail.com>
2024-10-18 17:25:26 +0100

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,5 +2,10 @@ export default function taskBlock(trueOFalse) {
   const task = false;
   const task2 = true;
 
-return trueOFalse ? [true, false] : [task, task2];
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
 }
